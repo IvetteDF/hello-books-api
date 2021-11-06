764b37f26bf2b9693253c40e843a0d83ae4d4939
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2021-11-06 15:08:36 -0500

    one to many relationship added author model

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import db
 from app.models.book import Book
+from app.models.author import Author
 from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
+authors_bp = Blueprint("authors", __name__, url_prefix="/authors")
 
 @books_bp.route("", methods=["GET", "POST"])
 def handle_books():
@@ -57,3 +59,58 @@ def handle_book(book_id):
         db.session.delete(book)
         db.session.commit()
         return make_response(f"Book #{book.id} successfully deleted")
+
+@authors_bp.route("", methods=["GET", "POST"])
+def handle_authors():
+    if request.method == "GET":
+        authors = Author.query_all()
+
+        authors_response = []
+        for author in authors:
+            authors_response.append({
+                "id" : author.id,
+                "name" : author.name
+            })
+        return jsonify(authors_response)
+        
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_author = Author(name=request_body["name"])
+
+        db.session.add(new_author)
+        db.session.commit()
+
+        return jsonify({
+            "id" : new_author.id,
+            "name" : new_author.name
+        }), 201
+
+@authors_bp.route("/<author_id>/books", methods=["GET", "POST"])
+def handle_authors_books(author_id):
+    author = Author.query.get(id=author_id)
+    if author is None:
+        return jsonify("Author not found"), 404
+
+    if request.method == "POST":
+        request_body = request.get_json()
+
+        new_book = Book(
+            title=request_body["title"],
+            description=request_body["description"], 
+            author=author
+        )
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return jsonify(f"Book {new_book.title} by {new_book.author}, successfully created"), 201
+
+    elif request.method == "GET":
+        books_response = []
+        for book in author.books:
+            books_response.append({
+                "id" : book.id,
+                "title" : book.title, 
+                "description" : book.description
+            })
+        return jsonify(books_response)
